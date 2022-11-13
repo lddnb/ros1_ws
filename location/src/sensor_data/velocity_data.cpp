cf6231b715ad995648f10f19189b9887e72f6818
lddnb <750126471@qq.com>
2022-11-13 22:30:52 +0800

    fix bug

--- location/src/sensor_data/velocity_data.cpp
+++ location/src/sensor_data/velocity_data.cpp
@@ -58,7 +58,7 @@ void VelocityData::TransformCoordinate(Eigen::Matrix4f transform_matrix) {
     Eigen::Vector3d delta_v;
     delta_v(0) = w(1) * r(2) - w(2) * r(1);
     delta_v(1) = w(2) * r(0) - w(0) * r(2);
-    delta_v(2) = w(1) * r(1) - w(1) * r(0);
+    delta_v(2) = w(0) * r(1) - w(1) * r(0);
     v = v + delta_v;
 
     angular_velocity.x = w(0);
